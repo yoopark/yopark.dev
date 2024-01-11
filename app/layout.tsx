9383adf90d1a9a6d9546b225415c4e176244f169
Yongjun Park <yopark@student.42seoul.kr>
2024-01-12 06:48:37 +0900

    perf: seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,14 @@ const pretendard = localFont({
 export const metadata: Metadata = {
   title: '박용준 · Yongjun Park',
   description: '프론트엔드 개발자 박용준의 포트폴리오입니다.',
+  openGraph: {
+    type: 'website',
+    locale: 'ko_KR',
+    title: '박용준 · Yongjun Park',
+    description: '프론트엔드 개발자 박용준의 포트폴리오입니다.',
+    url: 'https://yopark.dev',
+    images: 'https://yopark.dev/images/og-image.jpeg',
+  },
 };
 
 const RootLayout = ({ children }: React.PropsWithChildren) => {
