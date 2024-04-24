8bace6d6bed598002700ae0e6d62f5f3c1436b87
Yongjun Park <yopark@student.42seoul.kr>
2024-04-25 06:50:19 +0900

    fix: HomeHeader 세부 항목 제거 - tmp

--- app/components/HomeHeader/index.tsx
+++ app/components/HomeHeader/index.tsx
@@ -13,90 +13,90 @@ const links: NavigationLinkType[] = [
     title: 'Project',
     href: '#project',
     links: [
-      {
-        title: '블꾸',
-        href: '#blccu',
-      },
-      {
-        title: 'PlusAlpha',
-        href: '#plus-alpha',
-      },
-      {
-        title: '42Stat',
-        href: '#42-stat',
-      },
-      {
-        title: 'LastPong',
-        href: '#last-pong',
-      },
-      {
-        title: '밀리루틴',
-        href: '#miliroutine',
-      },
+      // {
+      //   title: '블꾸',
+      //   href: '#blccu',
+      // },
+      // {
+      //   title: 'PlusAlpha',
+      //   href: '#plus-alpha',
+      // },
+      // {
+      //   title: '42Stat',
+      //   href: '#42-stat',
+      // },
+      // {
+      //   title: 'LastPong',
+      //   href: '#last-pong',
+      // },
+      // {
+      //   title: '밀리루틴',
+      //   href: '#miliroutine',
+      // },
     ],
   },
   {
     title: 'Toy Project',
     href: '#toy-project',
     links: [
-      {
-        title: '극락 퀴즈쇼',
-        href: '#keukrak',
-      },
-      {
-        title: '개인 개발 블로그',
-        href: '#blog-yopark-dev',
-      },
-      {
-        title: '개인 포트폴리오 사이트',
-        href: '#yopark-dev',
-      },
-      {
-        title: 'Beforeleave',
-        href: '#beforeleave',
-      },
-      {
-        title: 'Cerebri Class',
-        href: '#cerebri-class',
-      },
+      // {
+      //   title: '극락 퀴즈쇼',
+      //   href: '#keukrak',
+      // },
+      // {
+      //   title: '개인 개발 블로그',
+      //   href: '#blog-yopark-dev',
+      // },
+      // {
+      //   title: '개인 포트폴리오 사이트',
+      //   href: '#yopark-dev',
+      // },
+      // {
+      //   title: 'Beforeleave',
+      //   href: '#beforeleave',
+      // },
+      // {
+      //   title: 'Cerebri Class',
+      //   href: '#cerebri-class',
+      // },
     ],
   },
   {
     title: 'Education',
     href: '#education',
     links: [
-      {
-        title: '고려대학교',
-        href: '#korea-univ',
-      },
-      {
-        title: '42서울',
-        href: '#42-seoul',
-      },
+      // {
+      //   title: '고려대학교',
+      //   href: '#korea-univ',
+      // },
+      // {
+      //   title: '42서울',
+      //   href: '#42-seoul',
+      // },
     ],
   },
   {
     title: 'Study',
     href: '#study',
     links: [
-      {
-        title: '우테코 따라잡기 (근데 이제 Next.js를 곁들인)',
-        href: '#not-woowacourse',
-      },
+      // {
+      //   title: '우테코 따라잡기 (근데 이제 Next.js를 곁들인)',
+      //   href: '#not-woowacourse',
+      // },
     ],
   },
   {
     title: 'Certificate',
     href: '#certificate',
     links: [
-      {
-        title: 'PCCP Python3',
-        href: '#pccp-python3',
-      },
-      {
-        title: '컴퓨터활용능력',
-        href: '#computer-specialist',
-      },
+      // {
+      //   title: 'PCCP Python3',
+      //   href: '#pccp-python3',
+      // },
+      // {
+      //   title: '컴퓨터활용능력',
+      //   href: '#computer-specialist',
+      // },
     ],
   },
 ];
